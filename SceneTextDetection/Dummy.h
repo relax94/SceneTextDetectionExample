cce9bb7b306cfc4062d86bb22d589c36e7f1bdb6
Dmitry Pavlenko <basket.pavlenko@gmail.com>
2018-10-04 16:49:03 +0300

    feature(external-libs): add tesseract and ocr libs for tests

---
+++ SceneTextDetection/Dummy.h
@@ -0,0 +1,17 @@
+//
+//  Dummy.h
+//  SceneTextDetection
+//
+//  Created by Dmytro Pavlenko on 10/4/18.
+//  Copyright © 2018 Dmytro Pavlenko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Dummy : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SceneTextDetection/Dummy.m
@@ -0,0 +1,13 @@
+//
+//  Dummy.m
+//  SceneTextDetection
+//
+//  Created by Dmytro Pavlenko on 10/4/18.
+//  Copyright © 2018 Dmytro Pavlenko. All rights reserved.
+//
+
+#import "Dummy.h"
+
+@implementation Dummy
+
+@end
